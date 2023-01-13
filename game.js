b05f489c1807e1601d060e4db2864681879559ab
Murad <49407545+Murad14@users.noreply.github.com>
2023-01-13 21:27:29 +0600

    pattern feature done

---
+++ game.js
@@ -0,0 +1,10 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+
+function nextSequence(){
+    var randomNumber =  Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+}
+nextSequence();
