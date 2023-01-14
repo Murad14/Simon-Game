bc6f817aef9f41791b6341ca2e138ae0d3df6e60
Murad <49407545+Murad14@users.noreply.github.com>
2023-01-15 01:21:02 +0600

    animation added to button

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ $(".btn").click(function () {
     userClickedPattern.push(userChosenColour);
 
     playSound(userChosenColour);
+    animatePress(userChosenColour);
 });
 
 
@@ -25,6 +26,17 @@ function nextSequence() {
 function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
+
+}
+
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+
+
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+     }
+     ,100);
 
 }
 
