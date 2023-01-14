c5011c88089cb0a6ecee24f2ab9c7ce7476371bc
Murad <49407545+Murad14@users.noreply.github.com>
2023-01-14 21:05:23 +0600

    modified

--- game.js
+++ game.js
@@ -1,11 +1,19 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
+
+
+$("btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+  });
 
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
     // var audio = new Audio("sounds/"+ randomChosenColour + ".mp3");
@@ -19,5 +27,13 @@ function nextSequence() {
         audio.play();
 
     });
+   
+    
 }
+
+
+
+
 nextSequence();
+
+
